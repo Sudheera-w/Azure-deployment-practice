3b7be7929a02812a4b51fa75be7b3245d7113fc1
Sudheera Weerawardhana <stweerawardhana@gmail.com>
2026-03-19 22:31:55 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: "30px", fontFamily: "Arial" }}>
+      <h1>React Frontend Running</h1>
+      <p>CSP Lab 5</p>
+    </div>
+  );
+}
+
+export default App;
